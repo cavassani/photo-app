a0fa1a67ed59d1c2c8b0c539b86118d5d96ad940
Marcelo Cavassani <marceloscavassani@gmail.com>
2017-04-27 11:51:09 -0300

    adição de botao de login, logout e ajustes no email da aplicação, configuração pra heroku e local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,12 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.action_mailer_delivery_method = :smtp
+  config.action_mailer.default_url_options = {
+    :host => 'http://cavassani-photo-app.herokuapp.com/',
+    :protocol => 'https'
+  }
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
