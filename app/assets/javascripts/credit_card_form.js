1be02e5a9e8d96aedd21ef38ade4407031c3da36
Marcelo  Cavassani <marceloscavassani@gmail.com>
2017-05-23 00:16:45 -0300

    remoção de linha errada no javascript de cartão de credito do stripe

--- app/assets/javascripts/credit_card_form.js
+++ app/assets/javascripts/credit_card_form.js
@@ -38,7 +38,6 @@ $(document).ready(function() {
       token = response.id;
 
       $form.append($("<input type=\"hidden\" name=\"payment[token]\" />").val(token));
-
       $("[data-stripe=number]").remove();
       $("[data-stripe=cvv]").remove();
       $("[data-stripe=exp-year]").remove();
